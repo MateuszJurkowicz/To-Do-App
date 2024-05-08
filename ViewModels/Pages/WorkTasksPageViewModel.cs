f801935c73878def6cb0a4e3e80f471b0ffa098a
Mateusz Jurkowicz <82638@student.pb.edu.pl>
2024-05-08 13:50:45 +0200

    Added logic for viewmodel

---
+++ ViewModels/Pages/WorkTasksPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using To_Do_App.ViewModels.Controls;
+
+namespace To_Do_App.ViewModels.Pages
+{
+    class WorkTasksPageViewModel
+    {
+        public List<WorkTaskViewModel> WorkTasks { get; set; } = new List<WorkTaskViewModel>();
+        public string NewWorkTaskTitle {  get; set; }
+        public string NewWorkTaskDescription { get; set; }
+        private void AddNewTask() 
+        {
+            var newTask = new WorkTaskViewModel
+            {
+                Title = NewWorkTaskTitle,
+                Description = NewWorkTaskDescription,
+                CreatedDate = DateTime.Now
+            };
+            WorkTasks.Add(newTask);
+        }
+
+    }
+}
