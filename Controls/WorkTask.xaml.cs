e2559fecb67be8c325cce76e7d7e17b1245386d9
GabrysiaCz <gczarnowicz588@gmail.com>
2024-05-21 22:44:27 +0200

    application appearance modified

--- Controls/WorkTask.xaml.cs
+++ Controls/WorkTask.xaml.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace To_Do_App.Controls
 {
-    /// <summary>
-    /// Interaction logic for WorkTask.xaml
-    /// </summary>
     public partial class WorkTask : UserControl
     {
         public WorkTask()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
         }
     }
 }
